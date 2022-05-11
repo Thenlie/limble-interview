1be5964959f1e4ad912e43cdf08eadfd324ceef1
Leithen <leithen113@gmail.com>
2022-05-11 14:22:45 -0600

    Cleaning up code to start over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommentListComponent } from './comment-list/comment-list.component';
-import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentListComponent,
-    UserInputComponent
   ],
   imports: [
     BrowserModule,
