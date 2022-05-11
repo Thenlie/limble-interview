7aa609a1e3707d9570b66e2758039355b8e72139
Leithen <leithen113@gmail.com>
2022-05-11 13:27:45 -0600

    Created initial components. Learning some Angular fundemantals.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentListComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule,
